812bcab873fbfb5515561103770e2ba439dd72d8
Igor Tullio <igortullio@hotmail.com>
2022-06-25 08:34:56 -0300

    Add graphql codegen

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,29 +1,13 @@
-import { gql, useQuery } from '@apollo/client';
 import classNames from 'classnames';
-import { Lesson, LessonProps } from './Lesson';
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-      id
-      slug
-      title
-      lessonType
-      availableAt
-    }
-  }
-`;
-
-interface GetLessonQueryResponse {
-  lessons: LessonProps[];
-}
+import { useGetLessonsQuery } from '../graphql/generated';
+import { Lesson } from './Lesson';
 
 interface SidebarProps {
   open: boolean;
 }
 
 export function Sidebar({ open }: SidebarProps) {
-  const { data } = useQuery<GetLessonQueryResponse>(GET_LESSONS_QUERY);
+  const { data } = useGetLessonsQuery();
 
   return (
     <aside className={classNames('absolute z-50 h-full w-full bg-gray-700 p-6 border-l border-gray-600 md:h-auto md:relative md:block md:w-[348px] md:max-w-[33vw]', { hidden: open })}>
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,49 +1,21 @@
-import { gql, useQuery } from '@apollo/client';
 import '@vime/core/themes/default.css';
 import { DefaultUi, Player, Youtube } from '@vime/react';
 import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from 'phosphor-react';
+import { useGetLessonBySlugQuery } from '../graphql/generated';
 import { Footer } from './Footer';
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String) {
-    lesson(where: { slug: $slug }) {
-      videoId
-      title
-      description
-      teacher {
-        bio
-        avatarURL
-        name
-      }
-    }
-  }
-`;
-
-interface GetLessonBySlugResponse {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string;
-    };
-  };
-}
 
 interface VideoProps {
   lessonSlug: string;
 }
 
 export function Video({ lessonSlug }: VideoProps) {
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: lessonSlug,
     },
   });
 
-  if (!data) {
+  if (!data || !data.lesson) {
     return (
       <div className="flex-1">
         <p>Carregando...</p>
@@ -70,14 +42,16 @@ export function Video({ lessonSlug }: VideoProps) {
             <h1 className="text-2xl font-bold">{title}</h1>
             <p className="mt-1 md:mt-4 text-gray-200 leading-relaxed">{description}</p>
 
-            <div className="flex items-center gap-4 mt-4">
-              <img src={teacher.avatarURL} alt="" className="h-16 w-16 rounded-full border-2 border-blue-500" />
+            {teacher && (
+              <div className="flex items-center gap-4 mt-4">
+                <img src={teacher.avatarURL} alt="" className="h-16 w-16 rounded-full border-2 border-blue-500" />
 
-              <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">{teacher.name}</strong>
-                <span className="text-gray-200 text-sm block">{teacher.bio}</span>
+                <div className="leading-relaxed">
+                  <strong className="font-bold text-2xl block">{teacher.name}</strong>
+                  <span className="text-gray-200 text-sm block">{teacher.bio}</span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
 
           <div className="flex flex-col w-full md:w-max gap-4">
